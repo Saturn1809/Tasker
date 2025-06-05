84c76f4e41d19a5b9104a5e6f45ca74e8b894781
saturn1809 <vyvipinyadav63@gmail.com>
2025-06-05 14:46:18 +0530

    second commit

---
+++ src/Wrapper.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Wrapper = (props) => {
+
+
+console.log(props);
+
+
+
+
+
+
+
+  return (
+   props.App
+  )
+}
+
+export default Wrapper
--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,5 +1,6 @@
 import { nanoid } from 'nanoid';
-import React, { useState } from 'react'
+import {useForm} from "react-hook-form";
+import { toast } from 'react-toastify';
 
 const Create = (props) => {
 
@@ -8,38 +9,37 @@ const settodos = props.settodos;
 
     
 
-const [title,settitle] = useState("");
-const Submiithandler = (e) =>{
+const {
 
- e.preventDefault();
+  register,
+  handleSubmit , 
+  reset, 
+  formState:{errors}} = useForm();
 
- const newtodo = {
+const Submiithandler = (data) =>{
 
-      id: nanoid(),
-      title,
-      isCompleted: false,
- };
 
- 
-//  const copytodos = [...todos];
-//  copytodos.push(newtodo)
-//  settodos(copytodos)
+ data.isCompleted=false;
+ data.id = nanoid();
 
- settodos([...todos,newtodo])
+ const copytodos = [...todos];
+ copytodos.push(data);
+ settodos(copytodos);
 
- settitle("");
- 
-
+ toast.success('Task Created')
+ reset();
 };
-
-
 
 
   return (
     <div className='border w-[70%] p-10   rounded-md bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-10 border border-gray-100'>
        <h1 className='text-5xl font-thin mb-14'>Set <span className='text-blue-700'> Reminders</span> for Tasks</h1>
-      <form onSubmit={Submiithandler} >
-        <input className='border-blue-300 border-b    focus:outline-none  py-2 w-full text-xl font-thin' onChange={(e)=> settitle(e.target.value)}  value={title} type="text" placeholder='Title' /> <br /><br />
+      <form onSubmit={handleSubmit(Submiithandler)} >
+        <input
+            {...register("title",{required:"Title can not be empty"})}
+             className='border-blue-300 border-b focus:outline-none  py-2 w-full text-xl font-thin'  type="text" placeholder='Title' />
+             {errors  && errors.title && errors.title.message && <small>{errors.title.message}</small> }
+             <br /><br />
         <button className='cursor-pointer text-blue-400  text-md border-1 p-2 tracking-tight rounded-xl border-blue-300 active:scale-95 transition hover:bg-blue-100'>Create Task</button>
       </form>
     </div>
